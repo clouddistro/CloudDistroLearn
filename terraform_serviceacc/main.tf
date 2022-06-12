6968dab9da9bb03ea075d25d1a812a6775f447ed
clouddistror <clouddistrolearn@gmail.com>
2022-06-12 13:33:27 +0000

    init

---
+++ terraform_serviceacc/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.24.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-infralearns"
+  region = "asia-south1"
+  zone = "asia-south1-a"
+  credentials = "terraform-infralearns-540465081d5b.json"
+}
+
+resource "google_compute_network" "custom_vpc_net" {
+        name = "vpcnetlearncustomtf"
+        auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "custom_vpc_subnet" {
+        name = "subvpcnetlearncustomtf"
+        network = google_compute_network.custom_vpc_net.id
+        ip_cidr_range = "22.0.0.0/24"
+        region = "asia-south1"
+}
+
+resource "google_compute_subnetwork" "custom_vpc_subnet_sngpr" {
+        name = "subvpcnetlearncustomtf"
+        network = google_compute_network.custom_vpc_net.id
+        ip_cidr_range = "22.1.0.0/24"
+        region = "asia-southeast1"
+}
+
+output "customvpc"{
+    value = google_compute_network.custom_vpc_net.id
+}
+
+resource "google_storage_bucket" "GCS_sh_acc_test1" {
+    name = "tf_bucket_sh_acc_test"
+    location = "ASIA"
+    uniform_bucket_level_access = true
+    //storage_class = "Multi-region"
+    lifecycle_rule {
+        condition {
+          age = 1
+        }
+        action{
+            type = "SetStorageClass"
+            storage_class = "COLDLINE"
+        }
+    }
+    retention_policy{
+      is_locked = true
+      retention_period = 864000
+    }
+}
